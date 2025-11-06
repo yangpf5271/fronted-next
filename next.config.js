be988bb0182138a95f1e68174abf81cb6c3dc0a7
liym <50445577+1723229@users.noreply.github.com>
2025-11-06 11:16:40 +0800

    修改端口

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     serverActions: true,
   },
   env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://10.0.0.87:7777',
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL ,
   },
   images: {
     domains: ['localhost', '10.0.0.87'], // 根据需要添加
